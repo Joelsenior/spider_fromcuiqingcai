87188c55e5a75a797c61d3386802f9169cbeb315
JOel Maisel <1279767778@qq.com>
2023-02-10 17:00:55 +0800

    完成了urllib--cookies

---
+++ 0210-1.py
@@ -0,0 +1,8 @@
+import urllib.request
+
+response = urllib.request.urlopen('https://www.python.org')
+# print(response.read().decode('utf-8'))
+# print(type(response))
+print(response.status)
+print(response.getheaders())
+print(response.getheader('Server'))
---
+++ 0210-7-cookies.py
@@ -0,0 +1,19 @@
+# import urllib.request, http.cookiejar
+
+# filename = 'cookie.txt'
+# # cookie = http.cookiejar.MozillaCookieJar(filename)
+# cookie = http.cookiejar.LWPCookieJar(filename)
+# handler = urllib.request.HTTPCookieProcessor(cookie)
+# opener = urllib.request.build_opener(handler)
+# response = opener.open('https://www.baidu.com')
+# cookie.save(ignore_discard=True, ignore_expires=True)
+
+
+import urllib.request, http.cookiejar
+
+cookie = http.cookiejar.LWPCookieJar()
+cookie.load('cookie.txt', ignore_discard=True, ignore_expires=True)
+handler = urllib.request.HTTPCookieProcessor(cookie)
+opener = urllib.request.build_opener(handler)
+response = opener.open('https://www.baidu.com')
+print(response.read().decode('utf-8'))
